014604428d9424074249aa248517066094fdfe5c
Saki Ebina <ebiebi0u0@gmail.com>
2024-09-17 16:13:15 -0700

    finished

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
 class Calculator {
+  constructor() {
+    this.num1Element = document.querySelector('#num1');
+    this.num2Element = document.querySelector('#num2');
+    this.operationElement = document.querySelector('#operation');
+    this.calculateBtn = document.querySelector('#calculateBtn');
+    this.resultElement = document.querySelector('#result');
 
+    
+    this.calculateBtn.addEventListener('click', () => this.compute());
+  }
+
+ add(num1,num2) {
+  return num1 + num2;
+ }
+ subtract(num1,num2){
+  return num1 - num2;
+ }
+ multiply(num1,num2){
+  return num1 * num2;
+ }
+ divide() {
+  if (num2 === 0) {
+    return 'Error: Division by zero';
+  }
+  return num1 / num2;
+  }
+
+  compute() {
+    const num1 = parseFloat(this.num1Element.value);
+    const num2 = parseFloat(this.num2Element.value);
+    const operation = this.operationElement.value;
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)) {
+      result = 'Please enter valid numbers!';
+    } else {
+      switch (operation) {
+        case 'add':
+          result = this.add(num1, num2);
+          break;
+        case 'subtract':
+          result = this.subtract(num1, num2);
+          break;
+        case 'multiply':
+          result = this.multiply(num1, num2);
+          break;
+        case 'divide':
+          result = this.divide(num1, num2);
+          break;
+        default:
+          result = 'Invalid operation';
+      }
+    }
+    this.resultElement.textContent = result;
+  }
 }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Calculator();
+});
+
+
